dbb1792ea00fcbf83ad308b563151f24d2323c58
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-12-23 06:57:16 +0000

    cubical graph

--- tests/test_generators.nim
+++ tests/test_generators.nim
@@ -260,6 +260,21 @@ test "generate bull graph":
   check G.numberOfEdges() == 5
   check G.edges() == @[(0, 1), (0, 2), (1, 2), (1, 3), (2, 4)]
 
+test "generate chvatal graph":
+  let G = chvatalGraph()
+  check G.isDirected() == false
+  check G.numberOfNodes() == 12
+  check G.numberOfEdges() == 24
+  check G.edges() == @[(0, 1), (0, 4), (0, 6), (0, 9), (1, 2), (1, 5), (1, 7), (2, 3), (2, 6), (2, 8), (3, 4), (3, 7), (3, 9), (4, 5), (4, 8), (5, 10), (5, 11), (6, 10), (6, 11), (7, 8), (7, 11), (8, 10), (9, 10), (9, 11)]
+
+test "generate cubical graph":
+  let G = cubicalGraph()
+  check G.isDirected() == false
+  check G.numberOfNodes() == 8
+  check G.numberOfEdges() == 12
+  check G.edges() == @[(0, 1), (0, 3), (0, 4), (1, 2), (1, 7), (2, 3), (2, 6), (3, 5), (4, 5), (4, 7), (5, 6), (6, 7)]
+
+
 # -------------------------------------------------------------------
 # TODO:
 # Random Graph
